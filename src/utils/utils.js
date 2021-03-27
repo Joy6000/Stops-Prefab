e99d1eb7cbc945cf1412aa24505f243be34291c0
Joy6000 <fnafgamer545@gmail.com>
2021-03-26 22:56:57 -0700

    Fixed a shitton of bugs

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ const loadCMDS = require('../handlers/command')
 const messageListener = require('../events/message')
 const ready = require('../events/ready')
 
-function Start (client, token, dir, handler) {
+function Start (client, dir, handler) {
     loadCMDS(dir, handler)
     client.on('message', message => {
         messageListener(client, message, handler)
