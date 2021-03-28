5f07778da68343ebc7a981d6340bcd400abd03ae
Joy6000 <fnafgamer545@gmail.com>
2021-03-28 12:59:26 -0700

    Utilize MongoURI

---
+++ src/utils/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+function connect (handler) {
+        await mongoose.connect(handler.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        })
+  return mongoose
+}
+async function connectToMongo(handler) {
+    await connect(handler).then(() => {
+        try {
+            console.log('Stop Prefab => Connected to mongo')
+        } catch (err) {
+            console.log('Stop Prefab => Failed to connect to mongo. Check your mongo URI.', err)
+        }
+    })
+}   
+
+module.exports = connectToMongo
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,13 @@
 const loadCMDS = require('../handlers/command')
 const loadEVNTS = require('../handlers/event')
+const connectToMongo = require('../utils/mongo')
 const messageListener = require('../events/message')
 const ready = require('../events/ready')
 
 async function Start (client, CMDdir, EVNDir, handler) {
     loadCMDS(CMDdir, handler)
     loadEVNTS(EVNDir, handler, client)
+    connectToMongo(handler)
     client.on('message', message => {
         messageListener(client, handler, message)
     })
